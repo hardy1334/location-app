7d20438f5c021ce1b25390485566babd4b443674
hardy1334 <hardychugh34@gmail.com>
2019-04-29 02:21:58 +0530

    react linked with node

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ app.get ('/', (req, res) => res.send ('hello'));
 
 app.use ('/api/admin', admins);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.listen (port, () => console.log ('Server running.....'));
